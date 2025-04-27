998fcb9f22b098bd2d8b86e4a33a7d0f86d00fdb
이건영 <dlrjsdud12752@naver.com>
2025-04-27 16:08:33 +0900

    base route

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { AppProvider } from "./Context";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AppProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/Thankschurch-main/">
         <Provider>
           <App />
         </Provider>
