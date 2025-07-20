92efdedb680298fcfe6f1664ed1a3255fc7d00c4
Thiziszero <dlrjsdud12752@naver.com>
2025-07-20 14:18:09 +0900

    제발git add .

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { AppProvider } from "./Context";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AppProvider>
-      <BrowserRouter basename="/Thankschurch-main/">
+      <BrowserRouter basename="/">
         <Provider>
           <App />
         </Provider>
